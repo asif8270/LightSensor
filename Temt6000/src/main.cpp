a8625ab384891499402ecd182a74e9db1f42ef5f
asif8270 <asifmehmood.8270@gmail.com>
2023-06-13 08:25:29 +0500

    Version: 2.0 Values can be calculated in both percentage & lux. Set the "x" true or false to see the result.

--- Temt6000/src/main.cpp
+++ Temt6000/src/main.cpp
@@ -1,5 +1,5 @@
 /*
-  Version: 1.0
+  Version: 2.0, Dated: 13/6/2023
   Sensor: TEMT6000 Ambient Light Sensor
   Authur: Asif Mahmood
   Designation: Embedded System Engineer
@@ -11,7 +11,8 @@
 const int signalPIN = GPIO_NUM_13;
 
 // function decleration
-void lightSensing();
+void lightPercent();
+void lightLux();
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,17 +22,38 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lightSensing();
-  delay(500);
+  // if x is true then light will be calculated in percentage else in lux
+
+  bool x = true; // x can be true or false
+  if(x){
+    lightPercent();
+    delay(500);
+  }
+  else{
+    lightLux();
+    delay(500);
+  }
+
 }
 
 // calculating light falling from source 
-void lightSensing(){
+
+// light in percentage
+void lightPercent(){  
   float lightVALUE;
   float light;
   lightVALUE = analogRead(signalPIN); // reading sensor's value (signal)
   light = lightVALUE*0.0976; // calculating light in percentage
-  Serial.println(light);
   Serial.print("Light ");
-  Serial.print("%:  ");
+  Serial.print(light);  
+  Serial.print(" %\n");
+}
+
+// light in lux
+void lightLux(){
+  float volts = map(analogRead(GPIO_NUM_13), 0, 1023, 0, 1000); // mapping analog values b/w 0-5 volts
+  float mAmps = (volts/10000)*1000; //  calculating mAmps I = (V/R)*1000
+  float lux = mAmps * 2.0; // calculating lux
+  Serial.print(lux);
+  Serial.print(" lux\n");
 }
